6f3847445c6cd48ffbbfb28eeda9097b1e97e673
Harshal Rembhotkar <harshal@b3networks.com>
2026-05-26 12:46:08 +0530

    deployong

--- render_start.sh
+++ render_start.sh
@@ -112,7 +112,7 @@ with open(sys.argv[1], "w", encoding="utf-8") as config_file:
 PY
 
 # Start OpenClaw first so the Python scripts can use `openclaw agent` locally.
-openclaw gateway --bind lan --port "$OPENCLAW_GATEWAY_PORT" --config "$OPENCLAW_CONFIG_PATH" --allow-unconfigured &
+openclaw gateway --bind lan --port "$OPENCLAW_GATEWAY_PORT" --allow-unconfigured &
 OPENCLAW_PID=$!
 
 
